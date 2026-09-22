2c5f326b9924242b03d62df733565f94ff82c337
MinK <2409120002@students.stamford.edu>
2026-09-22 21:03:03 +0700

    Add Offering model for project requirements

---
+++ server/models/Offering.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+
+const offeringSchema = new mongoose.Schema({
+course: {
+type: mongoose.Schema.Types.ObjectId,
+ref: "Course",
+required: true
+},
+
+term: {
+type: String,
+required: true,
+trim: true
+},
+
+section: {
+type: String,
+required: true,
+trim: true
+},
+
+instructor: {
+type: String,
+required: true,
+trim: true
+},
+
+day: {
+type: String,
+required: true,
+trim: true
+},
+
+startTime: {
+type: String,
+required: true
+},
+
+endTime: {
+type: String,
+required: true
+},
+
+room: {
+type: String,
+required: true,
+trim: true
+},
+
+capacity: {
+type: Number,
+required: true,
+min: 1
+},
+
+seatsTaken: {
+type: Number,
+default: 0,
+min: 0
+},
+
+addDropOpen: {
+type: Boolean,
+default: false
+},
+
+addDropCloseDate: {
+type: Date,
+default: null
+}
+});
+
+module.exports = mongoose.model("Offering", offeringSchema);
